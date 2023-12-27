f70c16e81191b76a38a205b3409b9e4a99584c11
simonpok <poksimon9@gmail.com>
2023-12-28 00:20:12 +0545

    Added a Geo-Location api

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//Using Geo-Location API
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    /*success Function*/ function (pos) {
+      console.log(pos);
+
+      //Destructuring ---> creates a variable out of the coords objects
+      const { latitude } = pos.coords;
+      const { longitude } = pos.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    /*Error Function*/ () => {
+      alert(`Sorry! Could not get your current location`);
+    }
+  );
